762a3ad9a74325ebbaec67a0f7594a8ff1dc8945
piyush11031 <pareekpiyush747@gmail.com>
2023-11-21 14:23:51 +0530

    cooo

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -17,8 +17,7 @@ import java.util.UUID;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-//this is the main branch code
-//second change to main branch code
+//This is v1 branch code
 public class BeerController {
 
     public static final String BEER_PATH = "/api/v1/beer";
